3516e5539758fe66e76a8af34632434ef359a80d
Ronald Chan <the@gr8.ninja>
2021-04-10 13:11:55 +0800

    add missing unit test

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ Docker
 Simply open the project in VSCode using the devcontainer and navigate to http://localhost:8080/doctor!
 
 # Alternatively
-- build the project for with `docker build --target prod -t ronaldslc/necktie-doctors-list-api src` in the project root. 
+- build the project for with `docker build --target prod -t ronaldslc/necktie-doctors-list-api -f docker/doctors-api/Dockerfile src` in the project root. 
 - run with `docker run -it -p 8000:8000 ronaldslc/necktie-doctors-list-api`
 - navigate to `http://localhost:8000/doctor/?district=SSP&price_range=1,1000`
+- test with `docker run -it ronaldslc/necktie-doctors-list-api -c "python manage.py test"`
--- src/doctors_service/doctors_api/tests.py
+++ src/doctors_service/doctors_api/tests.py
@@ -1,6 +1,41 @@
 """
 Test cases of Doctors Service
 """
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import DoctorSerializer
+from .models import Doctor
 
-# Create your tests here.
+
+# initialize the APIClient app
+client = Client()
+
+
+@api_view(['GET'])
+def get_doctor(request, pk):
+    try:
+        doctor = Doctor.objects.get(pk=pk)
+    except Doctor.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    # get details of a single puppy
+    if request.method == 'GET':
+        return Response({})
+
+
+class GetAllDoctorsTest(TestCase):
+    """ Test module for GET all doctors API """
+
+    fixtures = ["doctors.yaml"]
+
+    def test_get_all_doctors(self):
+        # get API response
+        response = client.get(reverse('doctor-list'))
+        # get data from db
+        doctors = Doctor.objects.all()
+        serializer = DoctorSerializer(doctors, many=True)
+        self.assertEqual(response.data['results'], serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
